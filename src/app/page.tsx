d83362b23f153e657cabc69bc319bde35616cc02
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-06-17 19:19:10 +1200

    modify some style to black border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
 
+
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-4">
-        <div>
-          <Button > I am a button </Button>
-        </div>
-        <div>
-          <Input placeholder="I am an input" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea value="I am a text area" />
-        </div>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+          <div>
+            <Button variant="destructive" > I am a button </Button>
+          </div>
+          <div>
+            <Input placeholder="I am an input" />
+          </div>
+          <div>
+            <Progress value={50} />
+          </div>
+          <div>
+            <Textarea placeholder="I am a text area" />
+          </div>
+          <div>
+            <Checkbox value={40} />
+          </div>
+      </div>
     </div>
       );
 }
