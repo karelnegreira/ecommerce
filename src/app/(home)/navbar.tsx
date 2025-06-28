3ad9fd7a93c4408084ae36af57eb8d0e035e42ee
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-06-28 22:50:08 +1200

    adding some styling to navbar

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,4 +1,6 @@
+import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google"
+import Link from "next/link";
 
 const poppins = Poppins({
     subsets: ["latin"], 
@@ -8,7 +10,11 @@ const poppins = Poppins({
 export const Navbar = () => {
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
-      
+      <Link href="/" className="pl-6">
+        <span className={cn("text-5xl font-semibold", poppins.className)}>
+            funroad
+        </span>
+      </Link>
     </nav>
   )
 }
