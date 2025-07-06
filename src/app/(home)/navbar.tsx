72a5f710a82b1076e28776d51653d6c606ee7fae
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-07-06 19:02:53 +1200

    navbar style added for buttons

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -18,10 +18,15 @@ interface NavbarItemProps {
 const NavbarItem = ({href, children, isActive} : NavbarItemProps) => {
     return (
         <Button 
+            asChild 
             variant="outline"
-            className={cn("bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent")}
+            className={cn("bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg", 
+            isActive && "bg-black text-white hover:bg-black hover:text-white", )}
         >
-            {children}
+            <Link href={href}>
+                 {children}
+            </Link>
+            
         </Button>
     );
 }
