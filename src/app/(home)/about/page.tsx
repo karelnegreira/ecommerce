9fa21e19acfc7a630743495262828fb9eb0a2bff
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-07-19 10:55:43 +1200

    folder routes added for each navbar link

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -3,7 +3,7 @@
 const Page = () => {
   return (
     <div>
-      
+        About page
     </div>
   )
 }
--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -38,6 +38,7 @@ const navbarItems = [
     {href: "/", children: "Home"},
     {href: "/about", children: "About"},
     {href: "/features", children: "Features"},
+    {href: "/pricing", children: "Pricing"},
     {href: "/contact", children: "Contact"},
 ]
 
