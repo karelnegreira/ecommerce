79ab1b85b5c4c2d571b37516283420ba02ab0188
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-10-05 04:04:56 +1200

    adding some style to search filter compoenent

--- src/app/(app)/(home)/search-filter/index.tsx
+++ src/app/(app)/(home)/search-filter/index.tsx
@@ -6,7 +6,7 @@ interface SearchFilterProps {
 
 export const SearchFilters = ({ data }: SearchFilterProps) => {
   return (
-    <div>
+    <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
       {JSON.stringify(data, null , 2)} 
     </div>
   );
