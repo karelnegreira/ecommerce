9dd465089ead400e7ddf4518efbaa3c66010d09b
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-10-05 04:19:42 +1200

    styling search icon with text input  now fits together and fancier

--- src/app/(app)/(home)/search-filter/search-input.tsx
+++ src/app/(app)/(home)/search-filter/search-input.tsx
@@ -9,8 +9,8 @@ export const SearchInput = ({disabled}: Props) => {
     return (
         <div className="flex items-center gap-2 w-full">
             <div className="relative w-full">
-                <SearchIcon className=""/>
-                <Input />
+                <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500"/>
+                <Input className="p-8" placeholder="Search products" disabled={disabled} />
             </div>
         </div>
     )
