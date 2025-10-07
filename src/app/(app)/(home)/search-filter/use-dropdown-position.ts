8ab96cbb44fc6493645b5c5ffcb32e3b57ac0438
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-10-08 05:38:27 +1200

    useDropdownPosition hook created

---
+++ src/app/(app)/(home)/search-filter/use-dropdown-position.ts
@@ -0,0 +1,11 @@
+import { RefObject } from "react";
+
+
+export const useDropdownPosition = (
+    ref: RefObject<HTMLDivElement | null> | RefObject<HTMLDivElement> ) => {
+        const getDropdownPosition = () => {
+
+        };
+
+        return {getDropdownPosition};
+    }
