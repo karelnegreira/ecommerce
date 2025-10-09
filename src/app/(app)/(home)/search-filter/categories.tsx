d69bc35624ac2706d5fc0411192dfbdf825ae50f
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-10-10 00:23:35 +1200

    categories dropped in screen

--- src/app/(app)/(home)/search-filter/categories.tsx
+++ src/app/(app)/(home)/search-filter/categories.tsx
@@ -7,19 +7,21 @@ interface CategoriesProps {
 }
 
 export const Categories = ({data}: CategoriesProps) => {
-    console.log({data}, 'TEST TO FIND THE BUG');
+    //console.log({data}, 'TEST TO FIND THE BUG');
   return (
-    <div>
-      {data.map((category: Category) => (
-        <div key={category.id}>
+    <div className="relative w-full">
+      <div className="flex flex-nowrap items-center">
+        {data.map((category: Category) => (
+          <div key={category.id}>
 
-            <CategoryDropdown  
-                category={category}
-                isActive={false}
-                isNavigationHovered={false}
-            />
-        </div>
-      ))}
+              <CategoryDropdown  
+                  category={category}
+                  isActive={false}
+                  isNavigationHovered={false}
+              />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
