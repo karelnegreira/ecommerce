cbf9ae8eb2c92b84ccd18cb2c68f3a09444310cf
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-11-01 02:57:41 +1200

    added routes to sign in in sign out pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () => {
+  return (
+    <div>
+      Sign in
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () => {
+  return (
+    <div>
+      Sign up
+    </div>
+  )
+}
+
+export default Page
