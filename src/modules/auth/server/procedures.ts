bf0395d3e18a4aff485f60bfa54dc6d8c72f4288
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-11-18 03:26:17 +1200

    rearranged route in project (app) / (auth) so fixws the layout bug can render now sign in and sign up pages

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -89,20 +89,10 @@ export const authRouter = createTRPCRouter({
             z.object({
                 email: z.string().email(), 
                 password: z.string(), 
-                username: z.string()
-                            .min(3, "Username must be at least 3 characters")
-                            .max(63, "Username must be at most 63 characters")
-                            .regex(
-                                /^[a-z0-9][a-z0-9-]*[a-z0-9]$/, 
-                                "Username must be only lower alphanumeric characters"
-                            )
-                            .refine(
-                                (val) => !val.includes("--"), 
-                                "username cannot contain consecutive hyphons"
-                            )
-                            .transform((val) => val.toLowerCase()), 
+                
             })
-        ).mutation(async ({input, ctx}) => {
+        )
+        .mutation(async ({input, ctx}) => {
             const data = await ctx.db.login({
                 collection: "users", 
                 data: {
