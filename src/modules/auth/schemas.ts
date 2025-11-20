cee0a770456e90fee5c2f6737c9fa1d4b0268012
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-11-20 23:33:23 +1200

    auth schema file added

---
+++ src/modules/auth/schemas.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const  registerSchemas = z.object({
+                email: z.string().email(), 
+                password: z.string(), 
+                username: z.string()
+                            .min(3, "Username must be at least 3 characters")
+                            .max(63, "Username must be at most 63 characters")
+                            .regex(
+                                /^[a-z0-9][a-z0-9-]*[a-z0-9]$/, 
+                                "Username must be only lower alphanumeric characters"
+                            )
+                            .refine(
+                                (val) => !val.includes("--"), 
+                                "username cannot contain consecutive hyphons"
+                            )
+                            .transform((val) => val.toLowerCase()), 
+            })
+
+    
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -4,6 +4,7 @@ import {zodResolver} from '@hookform/resolvers/zod';
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import {useForm} from 'react-hook-form';
 
 import {
     Form, 
