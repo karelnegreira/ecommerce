0e3bcf3067237b6a8c4768284884aab13b09539b
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-11-26 03:00:02 +1200

    abstract cookies

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -5,6 +5,7 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { TRPCError } from '@trpc/server';
 import { AUTH_COOKIE } from '../constants';
 import { loginSchema, registerSchema } from '../schemas';
+import { generateAuthCookie } from '../utils';
 
 
 
@@ -111,18 +112,11 @@ export const authRouter = createTRPCRouter({
                 });
             }
             //https://localhost:3000/api/users/login
-            const cookies = await getCookies();
-            cookies.set({
-                name: `${ctx.db.config.cookiePrefix}-token`, //payload-token by default  
-                value: data.token, 
-                httpOnly: true, 
-                path: "/", 
-                //sameSite: "none", 
-                //domain: ""
-            //TODO: Ensure cross-domain cookie sharing
-            //funroad.com // initial cookie
-            //karel.funroad.com cookie does not exist
-            }); 
+            
+            await generateAuthCookie({
+                prefix: ctx.db.config.cookiePrefix, 
+                value: data.token
+            })
            
             return data;
         }),
---
+++ src/modules/auth/utils.ts
@@ -0,0 +1,22 @@
+import {cookies as getCookies} from 'next/headers';
+import { AUTH_COOKIE } from './constants';
+
+interface Props {
+    prefix: string; 
+    value: string;
+};
+
+export const generateAuthCookie = async ({prefix, value} : Props) => {
+    const cookies = await getCookies();
+            cookies.set({
+                name: `${prefix}-token`, //payload-token by default  
+                value: value, 
+                httpOnly: true, 
+                path: "/", 
+                //sameSite: "none", 
+                //domain: ""
+            //TODO: Ensure cross-domain cookie sharing
+            //funroad.com // initial cookie
+            //karel.funroad.com cookie does not exist
+            }); 
+}
