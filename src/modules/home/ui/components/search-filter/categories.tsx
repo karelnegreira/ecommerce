59abe4e2fc630bccf029abd5f615c839b5c00083
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-05 16:22:20 +1200

    active category focus implemented

--- src/modules/home/ui/components/search-filter/categories.tsx
+++ src/modules/home/ui/components/search-filter/categories.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 import { CategoryDropdown } from "./category-dropdown"
 import { CategoriesSidebar } from "./categories-sidebar";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
+import { useParams } from "next/navigation";
 
 
 interface CategoriesProps {
@@ -16,6 +17,8 @@ interface CategoriesProps {
 }
 
 export const Categories = ({data}: CategoriesProps) => {
+
+    const params = useParams();
 
     const containerRef = useRef<HTMLDivElement>(null);
     const meassureRef = useRef<HTMLDivElement>(null);
@@ -25,7 +28,8 @@ export const Categories = ({data}: CategoriesProps) => {
     const [isAnyHovered, setIsAnyHovered] = useState(false);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const activeCategory = "all";
+    const categoryParam = params.category as string | undefined;
+    const activeCategory = categoryParam || "all";
 
     const activeCategoryIndex = data.findIndex((cat) => cat.slug === activeCategory);
     const isActiveCategoryHidden = activeCategoryIndex >= visibleCount && activeCategoryIndex !== -1;
