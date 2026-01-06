32a3da7dde33ebea999307d7b65c2ecef23bdde4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-07 00:04:58 +1200

    active category color effect now up and running

--- src/modules/home/ui/components/search-filter/index.tsx
+++ src/modules/home/ui/components/search-filter/index.tsx
@@ -5,8 +5,8 @@ import { useTRPC } from '@/trpc/client';
 import { Categories } from './categories';
 import { SearchInput } from "./search-input";
 import { useSuspenseQuery } from '@tanstack/react-query';
-
-
+import { useParams } from 'next/navigation';
+import { DEFAULT_BG_COLOR } from '../constants';
 
 export const SearchFilters = () => {
 
@@ -14,10 +14,24 @@ export const SearchFilters = () => {
   
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
 
+  const params = useParams();
+  const categoryParam = params.category as string || undefined;
+  const activeCategory = categoryParam || "all";
+
+  const activeCategoryData = data.find((category) => category.slug === activeCategory);
+
+  const activeCategoryColor  = activeCategoryData?.color || DEFAULT_BG_COLOR; 
+  const activeCategoryName = activeCategoryData?.name || null;
+
+  const activeSubcategory = params.subcategory as string | undefined;
+  const activeSubcategoryName = activeCategoryData?.subcategories?.find(
+    (subcategory) => subcategory.slug === activeSubcategory
+  )?.name || null;
+
 
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full" style={{
-      backgroundColor: "#F5F5F5", 
+      backgroundColor: activeCategoryColor, 
 
     }}>
         <SearchInput  />
