4e24ef3477e6032a0611571b1b4914954445857e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-15 00:56:24 +1200

    product is fetched by category it belongs to

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -10,17 +10,21 @@ interface Props {
     }>
 }; 
 
-const Page = async ({}: Props) => {
+const Page = async ({params}: Props) => {
+
+    const {category} = await params;
     
 
     const queryClient = getQueryClient();
     
-    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
+        category, 
+    }));
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback={<ProductListSkeleton/>}>
-              <ProductList />
+              <ProductList category={category} />
             </Suspense>
         </HydrationBoundary>
     )
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,14 +1,44 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { Category } from "@/payload-types";
+import z from "zod";
+
+import type { Where } from "payload";
 
 
 export const productsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async ( {ctx}) => {
+    getMany: baseProcedure.input(
+        z.object(
+            {
+                category: z.string().nullable().optional(), 
+            }
+        ), 
+    ).query(async ( {ctx, input}) => {
+        const where: Where = {}
 
+        if (input.category) {
+            const categoryData = await ctx.db.find({
+                collection: "categories", 
+                limit: 1, 
+                pagination: false, 
+                where: {
+                    slug: {
+                        equals: input.category, 
+                    }
+                }
+            });
+
+            const category = categoryData.docs[0];
+
+            if (category) {
+                where["category.slug"] = {
+                    equals: input.category, 
+                }
+            }
+        }
 
         const data = await ctx.db.find({
             collection: "products", 
             depth: 1,  //populate catgory and image  
+            where, 
           });
 
         //artificial delay for dev/test
