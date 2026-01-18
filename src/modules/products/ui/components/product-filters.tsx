e0fa5d585152bb12565cb7172ef6820279f19ec9
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-18 23:36:52 +1200

    price filter visible yet no function implemented

--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -1,6 +1,37 @@
 
 "use client";
 
+import { cn } from "@/lib/utils";
+import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
+
+interface ProductFilterProps {
+  title: string;
+  className?: string;
+  children: React.ReactNode
+}
+
+const ProductFilter = ({title, className, children}: ProductFilterProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const Icon = isOpen ? ChevronDownIcon : ChevronRightIcon;
+
+  return (
+    <div className={cn("p-4 border-b flex flex-col gap-2", className)}>
+      <div onClick={() => setIsOpen((current) => !current)}
+          className="flex items-center justify-between cursor-pointer"
+      >
+        <p className="font-medium">
+          {title}
+        </p>
+        <Icon className="size-5"/>
+      </div>
+      {isOpen && children}
+    </div>
+    );
+  
+}
+
 export const ProductFilters = () => {
   return (
     <div className="">
@@ -11,6 +42,9 @@ export const ProductFilters = () => {
               Clear
           </button>
         </div>
+        <ProductFilter title="Price" className="border-b-0">
+            <p>Price filter</p>
+        </ProductFilter>
       </div>
     </div>
   )
