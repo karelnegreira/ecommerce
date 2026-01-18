86515bd06fae4254c60188c17f40ffa355b612a4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-18 19:40:14 +1200

    dispolaying some product info with formatting

--- src/modules/products/ui/components/products-list.tsx
+++ src/modules/products/ui/components/products-list.tsx
@@ -15,8 +15,14 @@ export const ProductList = ({category}: Props) => {
     }));
 
     return (
-        <div> 
-            {JSON.stringify(data, null, 2)}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4"> 
+            {
+                data?.docs.map((product) => (
+                    <div key={product.id} className="border rounded-md bg-white">
+                            <h2>{product.name}</h2>
+                    </div>
+                ))
+            }
         </div>                                                                                                                                                                                                              
     );
 };
