b6f8e1490838ee6fabaa05740c96dca10bb7e1e6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-18 19:28:58 +1200

    adds some formatting to filters and product frontend

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -23,9 +23,21 @@ const Page = async ({params}: Props) => {
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<ProductListSkeleton/>}>
-              <ProductList category={category} />
-            </Suspense>
+            <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
+                
+                <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
+                    <div className="lg:col-span-2 xl:col-span-2">
+                        <div className="border p-2">
+                            Product filters
+                        </div>
+                    </div>
+                    <div className="lg:col-span-4 xl:col-span-6">
+                        <Suspense fallback={<ProductListSkeleton/>}>
+                            <ProductList category={category} />
+                        </Suspense>
+                    </div>
+                </div>
+            </div>  
         </HydrationBoundary>
     )
 }
