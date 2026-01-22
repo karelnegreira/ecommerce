ae5473bbac50795516add19acefec7e2654310d2
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-01-22 22:57:22 +1200

    tags api pagination coded

---
+++ src/modules/tags/server/procedures.ts
@@ -0,0 +1,26 @@
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import z from "zod";
+
+
+export const tagsRouter = createTRPCRouter({
+    getMany: baseProcedure.input(
+        z.object(
+            {
+                cursor: z.number().default(1), 
+                limit: z.number().default(10), 
+            }
+        ), 
+    ).query(async ( {ctx, input}) => {
+
+        const data = await ctx.db.find({
+            collection: "tags", 
+            page: input.cursor, 
+            limit: input.limit, 
+          });
+
+        //artificial delay for dev/test
+        //await new Promise((resolve) => setTimeout(resolve, 5000));
+
+        return data;
+    }),
+});
