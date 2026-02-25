03e366952035c300df4cd4149dbd7c807aad6279
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-02-26 10:42:59 +1200

    ProductSort component coded

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -8,6 +8,7 @@ import { Suspense } from "react";
 
 
 import { loadProductFilters } from "@/modules/products/search-params"; 
+import { ProductSort } from "@/modules/products/ui/components/product-sort";
 
 interface Props {
     params: Promise<{
@@ -38,7 +39,7 @@ const Page = async ({params, searchParams}: Props) => {
             <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
                 <div className="flex flex-col lg:flex-row lg:items-center gap-y-2 lg:gap-y-0 justify-between">
                     <p className="text-2xl font-medium">Curated for you</p>
-                    <p>SORTING</p>
+                    <ProductSort />
                 </div>
                 
                 <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
