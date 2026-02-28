8aa8dbcf15f63fce526e9874adef48839604ba65
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-02-28 17:07:22 +1200

    product card litle shadow effect now is visible

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -16,7 +16,7 @@ interface ProductCardProps {
 export const ProductCard = ({id, name, imageUrl, authorUsername, authorImageUrl, reviewRating, reviewCount, price}: ProductCardProps) => {
     return (
     <Link href="/">
-            <div className="border rounded-md bg-white overflow-hidden h-full flex flex-col">
+            <div className="hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-shadow border rounded-md bg-white overflow-hidden h-full flex flex-col">
                 <div className="relative aspect-square">
                     <Image 
                         alt={name}
