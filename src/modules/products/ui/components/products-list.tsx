fe2868cdb8e0649934ab53cd0c7406e973e698e7
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-08 02:57:49 +1200

    fancy loading message by stuling div

--- src/modules/products/ui/components/products-list.tsx
+++ src/modules/products/ui/components/products-list.tsx
@@ -79,7 +79,7 @@ export const ProductList = ({category}: Props) => {
 
 export const ProductListSkeleton = () => {
     return (
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
             Loading...
         </div>
     )
