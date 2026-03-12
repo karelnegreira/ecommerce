375732db755abf5f60689757b862cce6fc2f7d09
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-12 17:01:22 +1200

    importsmap command runned to fix bug and some new images uploaded

--- src/seed.ts
+++ src/seed.ts
@@ -73,6 +73,17 @@ const categories = [
   const seed  = async () => {
     const payload = await getPayload({ config });
 
+    //create admin user
+    await payload.create({
+      collection: "users", 
+      data: {
+        email: "admin@demo.com", 
+        password: "demo", 
+        roles: ["super-admin"],
+        username: "admin"
+      },
+    });
+
     for (const category of categories) {
         const parentCategory = await payload.create({
             collection: "categories", 
