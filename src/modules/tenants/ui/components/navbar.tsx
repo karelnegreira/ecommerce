b4a032990def344aede6df99bb3f6d59886623f6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-19 22:40:46 +1200

    generateTenantUrl function created

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { generateTenantUrl } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQueries, useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
@@ -15,7 +16,7 @@ export const Navbar = ({ slug }: Props) => {
     return (
         <nav className="h-20 border-b font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
-                <Link href={`/tenants/${slug}`} className="flex items-center gap-2">
+                <Link href={generateTenantUrl(slug)} className="flex items-center gap-2">
                     {data.image?.url && (
                         <Image
                             src={ data.image.url }
