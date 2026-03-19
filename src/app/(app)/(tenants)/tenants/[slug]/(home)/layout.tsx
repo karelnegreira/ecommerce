06de1f94b223005e4f680e24e0cbc8575daedb9c
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-19 19:22:05 +1200

    navbar component fetches name of tenant

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -1,15 +1,30 @@
+import { DEFAULT_LIMIT } from "@/constants";
 import { Footer } from "@/modules/tenants/ui/components/footer";
 import { Navbar } from "@/modules/tenants/ui/components/navbar";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { PathParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 interface LayoutProps {
     children: React.ReactNode;
     params: Promise<{slug: string}>;
 }
 
-const Layout = ({children, params}: LayoutProps) => {
+const Layout = async ({children, params}: LayoutProps) => {
+    const {slug} = await params;
+
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.tenants.getOne.queryOptions({
+        slug, 
+    }));
+
     return (
         <div className="min-h-screen bg-[#FFB6C1] flex flex-col">
-            <Navbar />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Navbar slug={slug}/>
+            </HydrationBoundary>
+            
             <div className="flex-1">
                 <div className="max-w-(--breakpoint-xl) mx-auto">
                     {children}
--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -1,9 +1,19 @@
+"use client";
 
-export const Navbar = () => {
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQueries, useSuspenseQuery } from "@tanstack/react-query";
+
+interface Props {
+    slug: string, 
+}
+
+export const Navbar = ({ slug }: Props) => {
+    const trpc = useTRPC();
+    const {data} = useSuspenseQuery(trpc.tenants.getOne.queryOptions({slug}));
     return (
         <nav className="h-20 border-b font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
-                <p className="text-xl">Tenant</p>
+                <p className="text-xl">{ data.name }</p>
             </div>
         </nav>
     );
