84e542ee4b57212b9f40b090022953fe15a20492
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-03-20 21:59:02 +1200

    final commit to tenant page branch, nawrro view now visible in product list skeleton

--- src/modules/products/ui/components/products-list.tsx
+++ src/modules/products/ui/components/products-list.tsx
@@ -1,20 +1,22 @@
 "use client";
 
 import { useTRPC } from "@/trpc/client"
-import { useSuspenseInfiniteQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { useProductFilters } from "../../hooks/use-product-filters";
 import { ProductCard, ProductCardSkeleton } from "./product-card";
 import { DEFAULT_LIMIT } from "@/constants";
 import { Button } from "@/components/ui/button";
 import { InboxIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 
 interface Props {
-    category?: string, 
-    tenantSlug?: string, 
+    category?: string; 
+    tenantSlug?: string; 
+    narrowView?: boolean;
 }
 
-export const ProductList = ({category, tenantSlug}: Props) => {
+export const ProductList = ({category, tenantSlug, narrowView}: Props) => {
 
     const [filters] = useProductFilters();
 
@@ -46,15 +48,17 @@ export const ProductList = ({category, tenantSlug}: Props) => {
 
     return (
         <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4"> 
+            <div className={cn("grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4", narrowView && "lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3")
+        
+            }> 
                 {data?.pages.flatMap((page) => page.docs).map((product) => (
                         <ProductCard 
                             key={product.id}
                             id={product.id}
                             name={product.name}
                             imageUrl={product.image?.url}
-                            authorUsername={product.tenant?.name}
-                            authorImageUrl={product.tenant?.image?.url}
+                            tenantSlug={product.tenant?.slug}
+                            tenantImageUrl={product.tenant?.image?.url}
                             reviewRating={3}
                             reviewCount={5}
                             price={product.price}
@@ -79,9 +83,10 @@ export const ProductList = ({category, tenantSlug}: Props) => {
     );
 };
 
-export const ProductListSkeleton = () => {
+export const ProductListSkeleton = ({narrowView}: Props) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        <div className={cn("grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4", narrowView && "lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3") 
+             }> 
             {Array.from({ length: DEFAULT_LIMIT}).map((_, index) => (
                 <ProductCardSkeleton key={index}/>
             ))}
