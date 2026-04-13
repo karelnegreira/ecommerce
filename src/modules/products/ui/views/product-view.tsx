1362a14978b2788e64dade33d8eb4bbb20b6fb61
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-13 23:16:21 +1200

    ProductView component added it yet being developed

---
+++ src/modules/products/ui/views/product-view.tsx
@@ -0,0 +1,12 @@
+interface ProductViewProps {
+    productId: string;
+    tenantSlug: string;
+}
+
+export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
