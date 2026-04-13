c3de117c035f32f7396e1be57c8e19ec588db47d
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-14 01:42:41 +1200

    productview component is being developed now some structure added and image visible

--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface ProductViewProps {
     productId: string;
     tenantSlug: string;
@@ -5,8 +7,17 @@ interface ProductViewProps {
 
 export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
     return (
-        <div>
-            
+        <div className="px-4 lg:px-12 py-10">
+            <div className="border rounded-sm bg-white overflow-hidden">
+                <div className="relative aspect-[3.9] border-b">
+                    <Image 
+                        src="/placeholder.png"
+                        alt="cover"
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
