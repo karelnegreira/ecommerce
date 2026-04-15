9926e16ba232a8f6e6a323f38541b318fa687261
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-04-15 23:02:39 +1200

    sellers profile picture and name  visiblein product view

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -18,11 +18,13 @@ export const productsRouter = createTRPCRouter({
             const product = await ctx.db.findByID({
                 collection: "products", 
                 id: input.id,
-            })
+                depth: 2,
+            });
 
             return {
                 ...product, 
                 image: product.image as Media | null, 
+                tenant: product.tenant as Tenant & {    image: Media | null},
             }
         }),
     getMany: baseProcedure.input(
--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 
-import { formatCurrency } from "@/lib/utils";
+import { formatCurrency, generateTenantUrl } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductViewProps {
     productId: string;
@@ -35,6 +36,24 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
                                 <div className="relative px-2 py-1 border bg-pink-400 w-fit">
                                     <p className="text-base font-medium">{formatCurrency(data.price)}</p>
                                 </div>
+                            </div>
+
+                            <div className="px-6 py-4 flex items-center justify-center lg:border-r">
+                                <Link href={generateTenantUrl(tenantSlug)} className="flex items-center gap-2">
+                                    {data.tenant.image?.url && (
+                                        <Image 
+                                            src={data.tenant.image.url}
+                                            alt={data.tenant.name}
+                                            width={20}
+                                            height={20}
+                                            className="rounded-full border shrink-0 size-[20px]"
+                                        />
+                                        )
+                                    }
+                                    <p className="text-base underline font-medium">
+                                        {data.tenant.name}
+                                    </p>
+                                </Link>
                             </div>
                         </div>
                     </div>
