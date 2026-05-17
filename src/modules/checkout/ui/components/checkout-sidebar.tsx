6ef89d92402118a973d87f958841ad6f4a4e61c8
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-05-18 00:36:38 +1200

    checlout button styled and visible at checkout sidebar

--- src/modules/checkout/ui/components/checkout-sidebar.tsx
+++ src/modules/checkout/ui/components/checkout-sidebar.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/lib/utils";
 
 
@@ -20,6 +21,17 @@ export const CheckoutSidebar = ({
             <div className="flex items-center justify-between p-4 border-b">
                 <h4 className="font-medium text-lg">Total</h4>
                 <p className="font-medium text-lg">{formatCurrency(total)}</p>
+            </div>
+            <div className="p-4 flex items-center justify-center">
+                <Button
+                    variant="elevated"
+                    disabled={isPending}
+                    onClick={onCheckout}
+                    size="lg"
+                    className="text-base w-full text-white bg-primary hover:bg-pink-400 hover:text-primary"
+                >
+                    Checkout
+                </Button>
             </div>
         </div>
     )
