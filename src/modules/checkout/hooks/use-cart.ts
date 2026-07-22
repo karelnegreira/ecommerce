a3d8d84c14b7a147d6e40018130af94ec5490320
Karel Negreira Fuentes <karelnegreira@gmail.com>
2026-07-23 05:59:22 +1200

    fixing bugs at use-cart hook

--- src/modules/checkout/hooks/use-cart.ts
+++ src/modules/checkout/hooks/use-cart.ts
@@ -1,23 +1,30 @@
+import { useCallback } from "react";
 import { useCartStore } from "../store/use-cart-store";
 
 export const useCart = (tenantSlug:string) => {
-    const {
-        getCartByTenant, 
-        addProduct, 
-        removeProduct, 
-        clearCart, 
+    /*const {
+        getCartByTenant,
+        addProduct,
+        removeProduct,
+        clearCart,
         clearAllCarts
-    } = useCartStore();
+    } = useCartStore();*/
+
+    const getCartByTenant = useCartStore((state) => state.getCartByTenant);
+    const addProduct = useCartStore((state) => state.addProduct);
+    const removeProduct = useCartStore((state) => state.removeProduct);
+    const clearCart = useCartStore((state) => state.clearCart);
+    const clearAllCarts = useCartStore((state) => state.clearAllCarts);
 
     const productIds = getCartByTenant(tenantSlug)
 
-    const toggleProducts = (productId: string) => {
+    const toggleProducts = useCallback((productId: string) => {
         if (productIds.includes(productId)) {
             removeProduct(tenantSlug, productId)
         } else {
             addProduct(tenantSlug, productId)
         }
-    };
+    }, [addProduct, removeProduct]);
 
     const isProductInCart = (productId: string) => {
         return productIds.includes(productId);
